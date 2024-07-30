4aee0f24bce3ac3105efc0238f5b2c1d598af218
Olekasndr Gubskiy <infernokgg@gmail.com>
2024-07-30 17:47:34 +0300

    feat: Created prisma model

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,10 +1,10 @@
-import express, { Express, Request, Response } from 'express';
-import 'dotenv/config';
 import bodyParser from 'body-parser';
+import 'dotenv/config';
+import express, { Express, Request, Response } from 'express';
 
 import AppRouter from './routes';
 
-const port = 3030;
+const port = 3031;
 const app: Express = express();
 const router = new AppRouter(app);
 
