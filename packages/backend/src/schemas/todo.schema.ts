55eca5de7b31c233f80c9f1bf855caa5268bef4c
IvanRuskevych <ivan.ruskevych@gmail.com>
2024-08-03 22:03:48 +0300

    fix: remove code from isExistMiddleware & fix todoSchema

--- packages/backend/src/schemas/todo.schema.ts
+++ packages/backend/src/schemas/todo.schema.ts
@@ -1,19 +1,20 @@
 import Joi from 'joi';
 import { joiErrorMessages } from '@/constants';
 
-export const todoSchema = Joi.object().keys({
-	title: Joi.string().required().messages(joiErrorMessages.string('Title')),
-	description: Joi.string()
-		.required()
-		.messages(joiErrorMessages.string('Description')),
-	isPrivate: Joi.boolean()
-		.required()
-		.messages(joiErrorMessages.boolean('Private/Public')),
-	isCompleted: Joi.boolean()
-		.required()
-		.messages(joiErrorMessages.boolean('Completed')),
-});
-
-export const reqBodySchema = Joi.object()
+export const todoSchema = Joi.object()
+	.keys({
+		title: Joi.string()
+			.required()
+			.messages(joiErrorMessages.string('Title')),
+		description: Joi.string()
+			.required()
+			.messages(joiErrorMessages.string('Description')),
+		isPrivate: Joi.boolean()
+			.required()
+			.messages(joiErrorMessages.boolean('Private/Public')),
+		isCompleted: Joi.boolean()
+			.required()
+			.messages(joiErrorMessages.boolean('Completed')),
+	})
 	.min(1)
 	.messages(joiErrorMessages.objMin());
