43a2dd610717bbc770bd993a2d248c440fd94815
Olekasndr Gubskiy <infernokgg@gmail.com>
2024-08-04 18:10:29 +0300

    feat: add relation between user and todos, update todos endpoints

--- packages/backend/src/controllers/todo.controller.ts
+++ packages/backend/src/controllers/todo.controller.ts
@@ -1,11 +1,13 @@
 import TodoService from '@/services/todo.service';
+import { UserType } from '@/types/user.types';
 import { Request, Response } from 'express';
 
 export class TodoController {
 	constructor(private todoService: TodoService) {}
 
-	async getAllTodo(_: Request, res: Response): Promise<void> {
-		const todos = await this.todoService.findAll();
+	async getAllTodo(req: Request, res: Response): Promise<void> {
+		const user = req.user as UserType;
+		const todos = await this.todoService.findAll(user);
 		res.send(todos);
 	}
 	async getTodoById(req: Request, res: Response): Promise<void> {
@@ -16,7 +18,8 @@ export class TodoController {
 	}
 
 	async create(req: Request, res: Response): Promise<void> {
-		const todo = await this.todoService.createTodo(req.body);
+		const user = req.user as UserType;
+		const todo = await this.todoService.createTodo(user, req.body);
 		res.send(todo);
 	}
 	async delete(req: Request, res: Response): Promise<void> {
--- packages/backend/src/routes/api/todos.route.ts
+++ packages/backend/src/routes/api/todos.route.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import { ctrlWrapper } from '@/helpers/ctrlWrapper';
+import { jwtAuth } from '@/middlewars/auth/jwtAuth';
 import { isExist } from '@/middlewars/isExist';
 import { validateBody } from '@/middlewars/validateBody';
 import { createTodoSchema, UpdateTodoSchema } from '@/schemas/todo.schema';
@@ -12,30 +13,31 @@ const todosRouter: Router = Router();
 
 todosRouter.get(
 	'/all',
+	jwtAuth,
 	ctrlWrapper(todoController.getAllTodo.bind(todoController)),
 );
 todosRouter.get(
 	'/:id',
-
+	jwtAuth,
 	isExist<TodoType>(prisma.todoItem),
 	ctrlWrapper(todoController.getTodoById.bind(todoController)),
 );
 todosRouter.post(
 	'/add',
-
+	jwtAuth,
 	validateBody(createTodoSchema),
 	ctrlWrapper(todoController.create.bind(todoController)),
 );
 todosRouter.delete(
 	'/:id',
-
+	jwtAuth,
 	isExist<TodoType>(prisma.todoItem),
 	ctrlWrapper(todoController.delete.bind(todoController)),
 );
 
 todosRouter.put(
 	'/:id',
-
+	jwtAuth,
 	isExist<TodoType>(prisma.todoItem),
 	validateBody(UpdateTodoSchema),
 	ctrlWrapper(todoController.update.bind(todoController)),
