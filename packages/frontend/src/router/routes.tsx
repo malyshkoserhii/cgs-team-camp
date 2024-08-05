52c3988ff3088d4a9f920dda3307559b74fa68b0
Olekasndr Gubskiy <infernokgg@gmail.com>
2024-08-05 20:39:03 +0300

    feat: add private/public routes logic

--- packages/frontend/src/router/routes.tsx
+++ packages/frontend/src/router/routes.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { ROUTER_KEYS } from '~shared/keys/router-keys';
+import { useAuthStore } from '~store/auth.store';
 
-export const publicRoutes = <>// PUBLIC_ROUTES</>;
+export const PrivateRoute = ({ element }): React.ReactNode => {
+	const { isLoggedIn } = useAuthStore();
+	const location = useLocation();
 
-export const privateRoutes = <>// PRIVATE_ROUTES</>;
+	return isLoggedIn ? (
+		element
+	) : (
+		<Navigate to={ROUTER_KEYS.LOGIN} state={{ from: location }} />
+	);
+};
+export const PublicRoute = ({
+	element,
+	redirectPath = '/',
+}): React.ReactNode => {
+	const { isLoggedIn } = useAuthStore();
+
+	return isLoggedIn ? <Navigate to={redirectPath} /> : element;
+};
