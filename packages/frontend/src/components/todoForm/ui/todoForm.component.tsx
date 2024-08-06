d9c7cb5ac99ea99b205f53e2d2ef5286d5a5cbbf
Vladyslav Yachyn <104788608+Reqvite@users.noreply.github.com>
2024-08-06 17:46:34 +0300

    updated todo form

--- packages/frontend/src/components/todoForm/ui/todoForm.component.tsx
+++ packages/frontend/src/components/todoForm/ui/todoForm.component.tsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import { TodoI } from '~shared/interfaces/todo.interface';
 import { todoSchemaResolver } from '~shared/joiSchemas/joiSchemas/todo/todo.schema';
 import { TodoFormModel } from '~shared/models/todo.model';
-import { useFilter } from '~shared/ui/filter/model/useFilter.hook';
 import { Form } from '~shared/ui/form';
 import { FormVariant } from '~shared/ui/form/ui/Form';
 import { useTodoStore } from '~store/todos.store';
@@ -20,21 +19,14 @@ export const TodoForm = ({
 	variant = 'default',
 	todo,
 }: Props): ReactElement => {
-	const {
-		fetchTodos,
-		updateTodoById,
-		createIsLoading,
-		editLoading,
-		createTodo,
-	} = useTodoStore();
+	const { updateTodoById, createIsLoading, editLoading, createTodo } =
+		useTodoStore();
 	const navigate = useNavigate();
-	const { params } = useFilter<TodoFormModel>();
 
 	const onSubmit = async (data: TodoFormModel): Promise<void> => {
 		data.isPrivate = JSON.parse(data.isPrivate);
 		if (isEdit) {
 			await updateTodoById(todo.id, data);
-			await fetchTodos(params);
 		} else {
 			await createTodo(data, navigate);
 		}
